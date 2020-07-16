db96ca8b4370d14aadf4a60887c3f18e54e3eb7b
zhangzheng521 <2215751025@qq.com>
2020-07-16 11:26:38 +0800

    【FIX】添加注册中心配置

--- src/main/java/com/itcast/minio/MinioApplication.java
+++ src/main/java/com/itcast/minio/MinioApplication.java
@@ -1,9 +1,13 @@
 package com.itcast.minio;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableSwagger2Doc
+@EnableDiscoveryClient
 public class MinioApplication {
 
     public static void main(String[] args) {
